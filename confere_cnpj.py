f797be57f84c997551bfb7f3f1270a604df6c7aa
Lucas Gomes <lucasmg@outlook.com.br>
2018-08-03 23:32:23 -0300

    arquivos para construção do BD pt 2

---
+++ confere_cnpj.py
@@ -0,0 +1,17 @@
+import requests
+import csv
+
+with open("postos_df_header.csv", 'r', newline='',encoding='latin-1') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=';')
+    header_row = next(csvreader)
+
+    for i in range(len(header_row)):
+        print(i, header_row[i], '\n')
+
+    for row in csvreader:
+        CNPJ = row[5]
+        print(CNPJ)
+
+    #r = requests.get('https://www.receitaws.com.br/v1/cnpj/27865757000102')
+
+    #print(r.text)
